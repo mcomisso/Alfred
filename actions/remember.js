216ed69c76635496a8dd4a74a9802b72655a01e2
Matteo Comisso <matteo.comisso@me.com>
2017-07-23 12:50:04 +0100

    Remember action wip

--- actions/remember.js
+++ actions/remember.js
@@ -1,21 +1,35 @@
 'use strict';
 
-exports.setupRememberAction = function (bot) {
+const chrono = require('chrono-node');
+const realm = require('../model');
+var CronJob = require('cron').CronJob;
+
+
+exports.registerRememberAction = function (bot) {
     bot.onText(/\/remember(.+)/, (msg, match) => {
 
         // memo
 
         // scheduler
 
-        var dateResults = chrono.parse(match.join(' '));
-        var text = dateResults[0].text;
+        let fullCommand = match.join(' ');
+
+        let dateResults = chrono.parse(fullCommand);
+        var timeString = dateResults[0].text;
         var date = dateResults[0].start.date();
 
         console.log(msg);
 
-        console.log(text);
-        console.log(msg);
+        console.log(timeString);
         console.log(date);
+
+        let cron = new CronJob(date, function () {
+
+            bot.sendMessage(msg.chat.id, message);
+        });
+
+        // Start the cronJob?
+        cron.start()
 
     });
 
