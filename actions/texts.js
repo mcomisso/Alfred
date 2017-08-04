a1e75017a3964c44a9c8e5dc8c653ec2a272ea1a
Matteo Comisso <matteo.comisso@me.com>
2017-08-04 21:45:56 +0100

    better matching

--- actions/texts.js
+++ actions/texts.js
@@ -8,7 +8,7 @@ exports.registerTextsAction = function (bot) {
     bot.on('message', function (msg) {
         // save the message in database
 
-        if (msg == "(╯°□°）╯︵ ┻━┻") {
+        if (msg.contains("┻━┻")) {
             bot.sendMessage(msg.chat.id, "┬─┬ ノ( ゜-゜ノ)");
         }
 
