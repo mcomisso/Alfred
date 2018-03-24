1b6ec84f0b777f4ddc7f11ab9d1b6d82f40f611f
Matteo Comisso <matteo.comisso@me.com>
2018-03-24 23:23:54 +0000

    Implement random word and gif

--- actions/random.js
+++ actions/random.js
@@ -1,29 +1,32 @@
 'use strict';
 
-const giphy = require('giphy-api')();
+const giphy = require('giphy-api')('wWv4Mbmn3Yyn77TWV6xtMGEhx4S6TyJd');
 const http = require('http');
 
-var reqOptions = {
-    host: 'setgetgo.com',
-    path: '/randomword/get.php'
-};
+const request = require('request');
 
-let callback = function(response) {
-    var str = '';
-
-    //another chunk of data has been received, so append it to `str`
-    response.on('data', function (chunk) {
-        str += chunk;
-    });
-
-    //the whole response has been received, so we just print it out here
-    response.on('end', function () {
-        console.log(str);
-    });
-}
+const WORD_API = "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
 exports.registerRandomAction = function (bot) {
     bot.onText(/\/random/, function (msg, match) {
-        http.request(reqOptions, callback).end();
+        request(WORD_API, (error, response, body) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            
+            if (response && response.statusCode == 200) {
+                let respJson = JSON.parse(body);
+                let word = respJson[0].word;
+
+                giphy.random('word', function (err, res) {
+                    if (err) {
+                        return;
+                    }
+                    bot.sendDocument(msg.chat.id, res.data.image_original_url, {caption: word});
+                    console.log(res);
+                });
+            }
+        });
     });
 }
