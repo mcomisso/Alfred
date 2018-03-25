a431c531b2d454f869dd0e5e340f54ddc5bd36c6
Matteo Comisso <matteo.comisso@me.com>
2018-03-25 21:18:12 +0100

    Gif improvements

--- actions/random.js
+++ actions/random.js
@@ -4,29 +4,53 @@ const giphy = require('giphy-api')('wWv4Mbmn3Yyn77TWV6xtMGEhx4S6TyJd');
 const http = require('http');
 
 const request = require('request');
+const _ = require('lodash');
 
 const WORD_API = "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
-exports.registerRandomAction = function (bot) {
-    bot.onText(/\/random/, function (msg, match) {
-        request(WORD_API, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            
-            if (response && response.statusCode == 200) {
-                let respJson = JSON.parse(body);
-                let word = respJson[0].word;
+var sendRandomGif = function(bot, msg, word) {
 
-                giphy.random('word', function (err, res) {
-                    if (err) {
-                        return;
-                    }
-                    bot.sendDocument(msg.chat.id, res.data.image_original_url, {caption: word});
-                    console.log(res);
-                });
-            }
-        });
+    request(WORD_API, (error, response, body) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        
+        if (response && response.statusCode == 200) {
+            let respJson = JSON.parse(body);
+            let word = respJson[0].word;
+
+            giphyFromWordToMessage(bot, word, msg);
+        }
+    });
+};
+
+var giphyFromWordToMessage = function(bot, word, msg) {
+    giphy.random(word, function (err, res) {
+        if (err) {
+            return;
+        }
+        bot.sendDocument(msg.chat.id, res.data.image_original_url, {caption: word});
+        console.log(res);
+    });
+};
+
+exports.registerRandomAction = function (bot) {
+
+    bot.onText(/\/gif(.+)?/, function (msg, match) { 
+        
+        // removes first element
+        match.shift();
+        
+        if (_.isEmpty(match)) {
+            sendRandomGif(bot, msg, undefined); 
+        } else {
+            let searchedWord = match.join(" ");
+            giphyFromWordToMessage(bot, searchedWord, msg);
+        }
+    });
+
+    bot.onText(/\/random/, function (msg, match) {
+        sendRandomGif(bot, msg, undefined);
     });
 }
